7d26da8015e17a9b7b4ca4e4b15216eff17514bb
jmgavino <gavinojohnmark9@gmail.com>
2026-04-10 00:25:00 +0800

    Create Repository.java

---
+++ Repository.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class HardwareRepository {
+
+    public static ArrayList<Hardware> getHardwareList() {
+        ArrayList<Hardware> list = new ArrayList<>();
+
+        // Laptops
+        list.add(new Laptop("Dell", 16));
+        list.add(new Laptop("HP", 32));
+        list.add(new Laptop("Lenovo", 16));
+        list.add(new Laptop("Asus", 8));
+
+        // Phones
+        list.add(new Phone("Samsung", 50));
+        list.add(new Phone("Apple", 12));
+        list.add(new Phone("Xiaomi", 50));
+        list.add(new Phone("Realme", 16));
+
+        return list;
+    }
+}

b9c6242a207292ebce1c981c704bc0b9202c883a
obuganjhaelordlaurence-a11y <obuganjhaelordlaurence@gmail.com>
2026-04-10 00:40:54 +0800

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Hardware> hardwareList = HardwareRepository.getHardwareList();
+
+        System.out.println("=== Hardware Masterlist ===");
+        for (Hardware hw : hardwareList) {
+            System.out.println(hw.getBrand() + " - " + hw.interpretSpec());
+        }
+
+        int laptop16 = 0;
+        int laptop32 = 0;
+        int phone50 = 0;
+
+        System.out.println("\n=== Inventory Count ===");
+
+        for (Hardware hw : hardwareList) {
+
+            if (hw instanceof Laptop) {
+                if (hw.getSpecValue() == 16) laptop16++;
+                if (hw.getSpecValue() == 32) laptop32++;
+            }
+
+            if (hw instanceof Phone) {
+                if (hw.getSpecValue() == 50) phone50++;
+            }
+        }
+
+        System.out.println("16GB Laptops: " + laptop16);
+        System.out.println("32GB Laptops: " + laptop32);
+        System.out.println("50MP Phones: " + phone50);
+    }
+}
